64a51edaace64ce270bfc67033c918c6bae285b7
JordyBushoff <Jordy_bushoff@Hotmail.com>
2021-04-22 16:54:46 +0200

    made first scene and uploaded packages
    - Background skybox package imported
    - gravity system of the meteors to the planets.
    - Spawn system of the meteors ( allmost done )

---
+++ PlanetaryClash/Assets/Scripts/Gravity/MeteorsGravityTo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorsGravityTo : MonoBehaviour
+{
+    public Transform gravityTarget;
+    public Transform gravityTarget2;
+
+    public float gravity = 9.81f;
+    Rigidbody rb;
+
+    public int maxRange;
+    public int minRange;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessGravity();
+    }
+
+
+    void ProcessGravity()
+    {
+        if(Vector3.Distance(gravityTarget.position, this.transform.position) < minRange)
+        {
+            Vector3 diff = transform.position - gravityTarget.position;
+            rb.AddForce(-diff.normalized * gravity * (rb.mass));
+            Debug.DrawRay(transform.position, diff.normalized, Color.red);
+        }
+
+        if (Vector3.Distance(gravityTarget2.position, this.transform.position) < minRange)
+        {
+            Vector3 diff = transform.position - gravityTarget2.position;
+            rb.AddForce(-diff.normalized * gravity * (rb.mass));
+            Debug.DrawRay(transform.position, diff.normalized, Color.red);
+        }
+        
+    }
+}
---
+++ PlanetaryClash/Assets/Scripts/Shoot/ShootFrom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFrom : MonoBehaviour
+{
+    public GameObject[] asteroids;
+
+    public GameObject[] spawnPositions;
+
+    public float spawnRate;
+    private float NextSpawn;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (NextSpawn > 0)
+            NextSpawn -= Time.deltaTime;
+        if (NextSpawn<=0)
+            Spawn();
+    }
+
+    private void Spawn()
+    {
+        NextSpawn=spawnRate;
+        Vector2 position = spawnPositions[Random.Range(0, spawnPositions.Length)].transform.position;
+        GameObject asteroidClone = Instantiate(asteroids[Random.Range(0, asteroids.Length)], new Vector2(position.x, position.y), transform.rotation);
+        asteroidClone.SetActive(true);
+    }
+}
